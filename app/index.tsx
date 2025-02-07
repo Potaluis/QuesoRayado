2b3146fc11ba30ddc75c1e1dd2cdae3d85a6af1a
Juan Luis <juanlupriv24@gmail.com>
2025-02-07 16:53:54 +0100

    Incluir permisos camara

--- app/index.tsx
+++ app/index.tsx
@@ -29,7 +29,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundImage: require('../assets/images/screamer.png'),
         height: '100%',
         width: '100%',
     },
